331fd90c674fe5ae9dd46997613c1538ce426861
ShMcK <me@shmck.com>
2015-08-07 17:54:52 +0900

    compile .ts & .ng.ts differently

--- plugin/handler.js
+++ plugin/handler.js
@@ -5,9 +5,14 @@ var typescript = Npm.require('typescript');
  * for Angular 2
  */
 var SETTINGS = {
-  module: typescript.ModuleKind.System,
-  emitDecoratorMetadata: true,
-  experimentalDecorators: true
+  ngTs: {
+    module: typescript.ModuleKind.System,
+    emitDecoratorMetadata: true,
+    experimentalDecorators: true
+  },
+  ts: {
+    module: typescript.ModuleKind.System
+  }
 };
 
 Plugin.registerSourceHandler('ts', function (compileStep) {
@@ -15,10 +20,9 @@ Plugin.registerSourceHandler('ts', function (compileStep) {
   // path to file from app root
   var inputPath = compileStep.inputPath;
 
-  // skip `.d.ts` files & handle only `.ng.ts` files
+  // skip `.d.ts` files
   var dTs = !!inputPath.match(new RegExp(/.d.ts$/i));
-  var ngTs = !!inputPath.match(new RegExp(/.ng.ts$/));
-  if (dTs || !ngTs) {
+  if (dTs) {
     // default TypeScript handling
     return true;
   }
@@ -28,17 +32,34 @@ Plugin.registerSourceHandler('ts', function (compileStep) {
   // sourcemaps file path
   var fileName = compileStep.pathForSourceMap;
 
-  // transpile TypeScript
-  var output = typescript.transpile(sourceCode, SETTINGS, fileName);
-  // register module with code
-  var moduleName = inputPath.replace(/\\/, '/').replace('.ng.ts', '');
+
+  // handle .ng.ts differently from .ts
+  var ngTs = !!inputPath.match(new RegExp(/.ng.ts$/)),
+    output, moduleName, newPath, moduleType;
+
+  if (ngTs) {
+    // transpile `.ng.ts` TypeScript
+    output = typescript.transpile(sourceCode, SETTINGS.ngTs, fileName);
+    // register module with code
+    moduleName = inputPath.replace(/\\/, '/').replace('.ng.ts', '');
+    newPath = inputPath.replace('.ng', '').replace('.ts', '.js');
+  } else {
+    // transpile `.ts` TypeScript
+    output = typescript.transpile(sourceCode, SETTINGS.ts);
+    // register module with code
+    moduleName = inputPath.replace(/\\/, '/').replace('.ts', '');
+    newPath = inputPath.replace('.ts', '.js');
+  }
+
+
   // register the module with System.js
   var data = output.replace("System.register([", 'System.register("' + moduleName + '",[');
+  console.log(data);
 
   // output
   compileStep.addJavaScript({
     // rename the file .js
-    path: inputPath.replace('.ng', '').replace('.ts', '.js'),
+    path: newPath,
     // output code
     data: data,
     // path to original `.ng.ts` file
